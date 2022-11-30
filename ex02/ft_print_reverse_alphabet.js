7ed2af289300d7287b3cb299609341b2d8072f24
margauxdeshayes <margaux.deshayes@icloud.com>
2022-11-30 18:52:19 +0100

    Modif ex02

--- ex02/ft_print_reverse_alphabet.js
+++ ex02/ft_print_reverse_alphabet.js
@@ -1,1 +1,12 @@
-console.log('zyxwvutsrqponmlkjihgfedcba')
+// console.log('zyxwvutsrqponmlkjihgfedcba')
+let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+function reverseAlphabet (tab) {
+    let tableau = tab
+    let string = tableau.toString().toLowerCase()
+    let split = string.split(",")
+    let reverse = split.reverse()
+    let join = reverse.join("")
+    return join
+}
+console.log (reverseAlphabet(letters))

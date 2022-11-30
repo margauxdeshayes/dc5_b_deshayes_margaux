5bd4b08affe3d50aadf14b32524ac01cf4390c7e
margauxdeshayes <margaux.deshayes@icloud.com>
2022-11-30 18:58:11 +0100

    modif ex03

--- ex03/ft_print_numbers.js
+++ ex03/ft_print_numbers.js
@@ -1,1 +1,12 @@
-console.log ('0123456789')
+// console.log ('0123456789')
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+
+function sortingNumb (tab) {
+    let tableau = tab
+    let string = tableau.toString().toLowerCase()
+    let split = string.split(",")
+    let sort = split.sort()
+    let join = sort.join("")
+    return join
+}
+console.log (sortingNumb(numbers))

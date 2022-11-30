d724fd9bbb6fce624761c47a5c244926c7e1012e
margauxdeshayes <margaux.deshayes@icloud.com>
2022-11-30 18:37:51 +0100

    Modif ex01

--- ex01/ft_print_alphabet.js
+++ ex01/ft_print_alphabet.js
@@ -1,1 +1,10 @@
-console.log ('abcdefghijklmnopqrstuvwxyz') 
+// console.log ('abcdefghijklmnopqrstuvwxyz') 
+let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+function alphabet (tab) {
+    let tableau = tab
+    let string = tableau.toString().toLowerCase()
+    let result = string.split(",")
+    result = result.join("")
+    return result;
+}
+console.log (alphabet(letters))
